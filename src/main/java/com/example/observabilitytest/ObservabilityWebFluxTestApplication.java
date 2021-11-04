3a37ce26a622a0dce71681cf5a6fd91e556f98af
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-11-04 19:00:07 +0100

    Making the sample work

--- src/main/java/com/example/observabilitytest/ObservabilityWebFluxTestApplication.java
+++ src/main/java/com/example/observabilitytest/ObservabilityWebFluxTestApplication.java
@@ -1,16 +1,25 @@
 package com.example.observabilitytest;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.OrderComparator;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.TimerRecordingListener;
 
 @SpringBootApplication
 public class ObservabilityWebFluxTestApplication {
@@ -37,10 +46,19 @@ public class ObservabilityWebFluxTestApplication {
 				.applicationStartup(new BufferingApplicationStartup(10_000))
 				.run(args);
 	}
-
+	
+	
 	@Bean
-	CommandLineRunner myCommandLineRunner() {
+	CommandLineRunner myCommandLineRunner(ListableBeanFactory beanFactory) {
 		return args -> {
+		    MeterRegistry registry = beanFactory.getBean(MeterRegistry.class);
+		    List<TimerRecordingListener> list = new ArrayList<>(beanFactory
+                    .getBeansOfType(TimerRecordingListener.class).values());
+		    Collections.reverse(list);
+            list.stream().forEach(listener -> registry.config()
+                    .timerRecordingListener(listener));
+		    
+		    
 			/*String object = restTemplate.getForObject("https://httpbin.org/headers", String.class);
 			if (!object.contains("B3")) {
 				throw new IllegalStateException("No B3 header propagated");
